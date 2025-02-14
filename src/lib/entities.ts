649aa6508b9f2b18db648a3bbe8ac0a2c443beb4
Reuben Reyes <reuben.a.reyes@gmail.com>
2025-02-13 18:44:30 -0800

    chore: remove weird prettierrc rules, reformat stuff

--- src/lib/entities.ts
+++ src/lib/entities.ts
@@ -1,6 +1,6 @@
 interface SpaceParams {
-	id: string;
-	name: string;
+  id: string;
+  name: string;
 }
 
 /**
@@ -8,27 +8,27 @@ interface SpaceParams {
  * `Spaces` don't exhibit any behavior; however, `Profiles` may belong to `Spaces`.
  */
 export class Space {
-	#id: string;
-	#name: string;
+  #id: string;
+  #name: string;
 
-	constructor({ id, name }: SpaceParams) {
-		this.#id = id;
-		this.#name = name;
-	}
+  constructor({ id, name }: SpaceParams) {
+    this.#id = id;
+    this.#name = name;
+  }
 
-	get id() {
-		return this.#id;
-	}
+  get id() {
+    return this.#id;
+  }
 
-	get name() {
-		return this.#name;
-	}
+  get name() {
+    return this.#name;
+  }
 }
 
 interface AccountParams {
-	id: string;
-	profiles: Profile[];
-	isBanned: boolean;
+  id: string;
+  profiles: Profile[];
+  isBanned: boolean;
 }
 
 /**
@@ -38,52 +38,52 @@ interface AccountParams {
  * only the `Account` owner may view their various `Profiles`.
  */
 export class Account {
-	#id: string;
-	#profiles: Profile[];
-	#isBanned: boolean;
+  #id: string;
+  #profiles: Profile[];
+  #isBanned: boolean;
 
-	constructor(params: AccountParams) {
-		this.#id = params.id;
-		this.#profiles = params.profiles;
-		this.#isBanned = params.isBanned;
-	}
+  constructor(params: AccountParams) {
+    this.#id = params.id;
+    this.#profiles = params.profiles;
+    this.#isBanned = params.isBanned;
+  }
 
-	get id() {
-		return this.#id;
-	}
+  get id() {
+    return this.#id;
+  }
 
-	/**
-	 * Gets a list of this `Account`'s `Profiles`. This method should NOT be callable
-	 * by anyone except the `Account` owner.
-	 */
-	get profiles() {
-		return this.#profiles;
-	}
+  /**
+   * Gets a list of this `Account`'s `Profiles`. This method should NOT be callable
+   * by anyone except the `Account` owner.
+   */
+  get profiles() {
+    return this.#profiles;
+  }
 
-	get isBanned() {
-		return this.#isBanned;
-	}
+  get isBanned() {
+    return this.#isBanned;
+  }
 
-	createProfile(params: { profileId: string; space: Space }) {
-		return new Profile({
-			id: params.profileId,
-			space: params.space,
-			exemplaryReplies: []
-		});
-	}
+  createProfile(params: { profileId: string; space: Space }) {
+    return new Profile({
+      id: params.profileId,
+      space: params.space,
+      exemplaryReplies: [],
+    });
+  }
 
-	ban() {
-		this.#isBanned = true;
-	}
+  ban() {
+    this.#isBanned = true;
+  }
 
-	unban() {
-		this.#isBanned = false;
-	}
+  unban() {
+    this.#isBanned = false;
+  }
 }
 
 interface ProfileParams {
-	id: string;
-	space: Space;
+  id: string;
+  space: Space;
 }
 
 /**
@@ -93,27 +93,27 @@ interface ProfileParams {
  * When interacting with others in a space, those interactions are tied to a _`Profile`_, not an `Account`.
  */
 export class Profile {
-	#id: string;
-	#space: Space;
+  #id: string;
+  #space: Space;
 
-	constructor(params: ProfileParams) {
-		this.#id = params.id;
-		this.#space = params.space;
-	}
+  constructor(params: ProfileParams) {
+    this.#id = params.id;
+    this.#space = params.space;
+  }
 
-	get id() {
-		return this.#id;
-	}
+  get id() {
+    return this.#id;
+  }
 
-	get space() {
-		return this.#space;
-	}
+  get space() {
+    return this.#space;
+  }
 }
 
 interface LetterParams {
-	id: string;
-	content: string;
-	author: Profile;
+  id: string;
+  content: string;
+  author: Profile;
 }
 
 /**
@@ -122,52 +122,52 @@ interface LetterParams {
  * associated to that `Conversation`.
  */
 export class Letter {
-	#id: string;
-	#content: string;
-	#author: Profile;
+  #id: string;
+  #content: string;
+  #author: Profile;
 
-	constructor(params: LetterParams) {
-		this.#id = params.id;
-		this.#content = params.content;
-		this.#author = params.author;
-	}
+  constructor(params: LetterParams) {
+    this.#id = params.id;
+    this.#content = params.content;
+    this.#author = params.author;
+  }
 
-	get id() {
-		return this.#id;
-	}
+  get id() {
+    return this.#id;
+  }
 
-	get content() {
-		return this.#content;
-	}
+  get content() {
+    return this.#content;
+  }
 
-	get author() {
-		return this.#author;
-	}
+  get author() {
+    return this.#author;
+  }
 
-	/**
-	 * Starts a new conversation pertaining to this letter.
-	 * The only profiles who can interact with this conversation are the letter author
-	 * and the responder sent in this method.
-	 */
-	startNewConversation(params: { conversationId: string; responder: Profile }) {
-		const conversation = new Conversation({
-			id: params.conversationId,
-			letter: this,
-			responder: params.responder,
-			createdAt: new Date()
-		});
+  /**
+   * Starts a new conversation pertaining to this letter.
+   * The only profiles who can interact with this conversation are the letter author
+   * and the responder sent in this method.
+   */
+  startNewConversation(params: { conversationId: string; responder: Profile }) {
+    const conversation = new Conversation({
+      id: params.conversationId,
+      letter: this,
+      responder: params.responder,
+      createdAt: new Date(),
+    });
 
-		return conversation;
-	}
+    return conversation;
+  }
 }
 
 interface ConversationParams {
-	id: string;
-	createdAt: Date;
-	letter: Letter;
-	responder: Profile;
-	isPublicationRequested?: boolean;
-	isPublicationAccepted?: boolean;
+  id: string;
+  createdAt: Date;
+  letter: Letter;
+  responder: Profile;
+  isPublicationRequested?: boolean;
+  isPublicationAccepted?: boolean;
 }
 
 /**
@@ -180,90 +180,90 @@ interface ConversationParams {
  * by either the sender or the responder.
  */
 export class Conversation {
-	#id: string;
-	#createdAt: Date;
-	#letter: Letter;
-	#responder: Profile;
-	#isPublicationRequested: boolean;
-	#isPublicationAccepted: boolean;
+  #id: string;
+  #createdAt: Date;
+  #letter: Letter;
+  #responder: Profile;
+  #isPublicationRequested: boolean;
+  #isPublicationAccepted: boolean;
 
-	constructor(params: ConversationParams) {
-		this.#id = params.id;
-		this.#createdAt = params.createdAt;
-		this.#letter = params.letter;
-		this.#responder = params.responder;
-		this.#isPublicationRequested = params.isPublicationRequested ?? false;
-		this.#isPublicationAccepted = params.isPublicationAccepted ?? false;
-	}
+  constructor(params: ConversationParams) {
+    this.#id = params.id;
+    this.#createdAt = params.createdAt;
+    this.#letter = params.letter;
+    this.#responder = params.responder;
+    this.#isPublicationRequested = params.isPublicationRequested ?? false;
+    this.#isPublicationAccepted = params.isPublicationAccepted ?? false;
+  }
 
-	get id() {
-		return this.#id;
-	}
+  get id() {
+    return this.#id;
+  }
 
-	get createdAt() {
-		return this.#createdAt;
-	}
+  get createdAt() {
+    return this.#createdAt;
+  }
 
-	get letter() {
-		return this.#letter;
-	}
+  get letter() {
+    return this.#letter;
+  }
 
-	get responder() {
-		return this.#responder;
-	}
+  get responder() {
+    return this.#responder;
+  }
 
-	get isPublic() {
-		return this.#isPublicationRequested && this.#isPublicationAccepted;
-	}
+  get isPublic() {
+    return this.#isPublicationRequested && this.#isPublicationAccepted;
+  }
 
-	requestPublication() {
-		this.#isPublicationRequested = true;
-	}
+  requestPublication() {
+    this.#isPublicationRequested = true;
+  }
 
-	rescindPublicationRequest() {
-		this.#isPublicationRequested = false;
-	}
+  rescindPublicationRequest() {
+    this.#isPublicationRequested = false;
+  }
 
-	acceptPublication() {
-		this.#isPublicationAccepted = true;
-	}
+  acceptPublication() {
+    this.#isPublicationAccepted = true;
+  }
 
-	rescindPublicationAcceptance() {
-		this.#isPublicationAccepted = false;
-	}
+  rescindPublicationAcceptance() {
+    this.#isPublicationAccepted = false;
+  }
 
-	/**
-	 * Sends a reply to this conversation.
-	 */
-	sendReply(params: {
-		conversation: Conversation;
-		responder: Profile;
-		response: string;
-		replyId: string;
-		replySentAt: Date;
-	}) {
-		const reply = new Reply({
-			id: params.replyId,
-			conversation: params.conversation,
-			content: params.response,
-			author: params.responder,
-			createdAt: new Date(),
-			sentAt: params.replySentAt
-		});
+  /**
+   * Sends a reply to this conversation.
+   */
+  sendReply(params: {
+    conversation: Conversation;
+    responder: Profile;
+    response: string;
+    replyId: string;
+    replySentAt: Date;
+  }) {
+    const reply = new Reply({
+      id: params.replyId,
+      conversation: params.conversation,
+      content: params.response,
+      author: params.responder,
+      createdAt: new Date(),
+      sentAt: params.replySentAt,
+    });
 
-		return reply;
-	}
+    return reply;
+  }
 }
 
 interface ReplyParams {
-	id: string;
-	conversation: Conversation;
-	author: Profile;
-	content: string;
-	sentAt: Date;
-	createdAt: Date;
-	reaction?: string;
-	label?: Label;
+  id: string;
+  conversation: Conversation;
+  author: Profile;
+  content: string;
+  sentAt: Date;
+  createdAt: Date;
+  reaction?: string;
+  label?: Label;
 }
 
 /**
@@ -274,77 +274,77 @@ interface ReplyParams {
  * and "labels", specific enumerated tags which speak to the quality of the reply.
  */
 export class Reply {
-	#id: string;
-	#conversation: Conversation;
-	#author: Profile;
-	#content: string;
-	#sentAt: Date;
-	#createdAt: Date;
-	#reaction?: string;
-	#label?: Label;
+  #id: string;
+  #conversation: Conversation;
+  #author: Profile;
+  #content: string;
+  #sentAt: Date;
+  #createdAt: Date;
+  #reaction?: string;
+  #label?: Label;
 
-	constructor(params: ReplyParams) {
-		this.#id = params.id;
-		this.#conversation = params.conversation;
-		this.#author = params.author;
-		this.#content = params.content;
-		this.#sentAt = params.sentAt;
-		this.#createdAt = params.createdAt;
-		this.#reaction = params.reaction;
-		this.#label = params.label;
-	}
+  constructor(params: ReplyParams) {
+    this.#id = params.id;
+    this.#conversation = params.conversation;
+    this.#author = params.author;
+    this.#content = params.content;
+    this.#sentAt = params.sentAt;
+    this.#createdAt = params.createdAt;
+    this.#reaction = params.reaction;
+    this.#label = params.label;
+  }
 
-	get id() {
-		return this.#id;
-	}
+  get id() {
+    return this.#id;
+  }
 
-	get conversation() {
-		return this.#conversation;
-	}
+  get conversation() {
+    return this.#conversation;
+  }
 
-	get author() {
-		return this.#author;
-	}
+  get author() {
+    return this.#author;
+  }
 
-	get content() {
-		return this.#content;
-	}
+  get content() {
+    return this.#content;
+  }
 
-	get sentAt() {
-		return this.#sentAt;
-	}
+  get sentAt() {
+    return this.#sentAt;
+  }
 
-	get createdAt() {
-		return this.#createdAt;
-	}
+  get createdAt() {
+    return this.#createdAt;
+  }
 
-	get reaction(): string | undefined {
-		return this.#reaction;
-	}
+  get reaction(): string | undefined {
+    return this.#reaction;
+  }
 
-	get label(): Label | undefined {
-		return this.#label;
-	}
+  get label(): Label | undefined {
+    return this.#label;
+  }
 
-	set reaction(r: string) {
-		this.#reaction = r;
-	}
+  set reaction(r: string) {
+    this.#reaction = r;
+  }
 
-	/**
-	 * Sets the label for this reply. If a label is already set, it cannot be overridden.
-	 */
-	set label(l: Label) {
-		if (this.#label !== undefined) {
-			// do nothing
-		}
+  /**
+   * Sets the label for this reply. If a label is already set, it cannot be overridden.
+   */
+  set label(l: Label) {
+    if (this.#label !== undefined) {
+      // do nothing
+    }
 
-		switch (l) {
-			case Label.Exemplary:
-				break;
-			default:
-				break;
-		}
-	}
+    switch (l) {
+      case Label.Exemplary:
+        break;
+      default:
+        break;
+    }
+  }
 }
 
 /**
@@ -352,21 +352,21 @@ export class Reply {
  * of that `Reply`'s contents.'
  */
 export enum Label {
-	/**
-	 * The `Exemplary` label indicates that the person who applied this label to a `Reply` found
-	 * that reply's contents significant to them. `Exemplary` labels are shown on the recipient's `Profile`.
-	 */
-	Exemplary,
-	/**
-	 * The `Helpful` label indicates that the person who applied this label to a `Reply` found
-	 * that reply's contents helpful to them.
-	 */
-	Helpful,
-	/**
-	 * The `Malice` label indicates that the person who applied this label to a `Reply` found
-	 * that reply's contents malicious. The sender of the `Reply` will have their responses
-	 * in the parent `Conversation` reviewed, and punishment  may be applied to their `Account`
-	 * based on further judgment.
-	 */
-	Malice
+  /**
+   * The `Exemplary` label indicates that the person who applied this label to a `Reply` found
+   * that reply's contents significant to them. `Exemplary` labels are shown on the recipient's `Profile`.
+   */
+  Exemplary,
+  /**
+   * The `Helpful` label indicates that the person who applied this label to a `Reply` found
+   * that reply's contents helpful to them.
+   */
+  Helpful,
+  /**
+   * The `Malice` label indicates that the person who applied this label to a `Reply` found
+   * that reply's contents malicious. The sender of the `Reply` will have their responses
+   * in the parent `Conversation` reviewed, and punishment  may be applied to their `Account`
+   * based on further judgment.
+   */
+  Malice,
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,69 +1,69 @@
 <script lang="ts">
-	export const spaces = [
-		{ id: 'space1', name: 'work', displayName: 'Work' },
-		{ id: 'space2', name: 'relationships', displayName: 'Relationships' },
-		{ id: 'space3', name: 'health', displayName: 'Health' }
-	];
+  export const spaces = [
+    { id: "space1", name: "work", displayName: "Work" },
+    { id: "space2", name: "relationships", displayName: "Relationships" },
+    { id: "space3", name: "health", displayName: "Health" },
+  ];
 </script>
 
 <svelte:head>
-	<link
-		href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
-		rel="stylesheet"
-	/>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Pixelify+Sans:wght@400..700&display=swap"
+    rel="stylesheet"
+  />
 </svelte:head>
 
 <main class="main">
-	<div class="card card--menu">
-		<h1>Words of Affirmation</h1>
-	</div>
+  <div class="card card--menu">
+    <h1>Words of Affirmation</h1>
+  </div>
 
-	<div class="card card--menu">
-		<p>What do you want to talk about?</p>
+  <div class="card card--menu">
+    <p>What do you want to talk about?</p>
 
-		<form action="" class="form form--select">
-			{#each spaces as space}
-				<input type="checkbox" id={space.id} class="form" />
-				<label for={space.id}>{space.displayName}</label>
-			{/each}
+    <form action="" class="form form--select">
+      {#each spaces as space}
+        <input type="checkbox" id={space.id} class="form" />
+        <label for={space.id}>{space.displayName}</label>
+      {/each}
 
-			<button type="button" class="button button--expandform">[more]</button>
-			<button type="submit">Done</button>
-		</form>
-	</div>
+      <button type="button" class="button button--expandform">[more]</button>
+      <button type="submit">Done</button>
+    </form>
+  </div>
 </main>
 
 <style>
-	main.main {
-		display: grid;
-		width: 100vw;
-		height: 100vh;
-		align-content: center;
-		place-items: center;
-		font-family: 'Pixelify Sans', serif;
-		background-color: #111;
-		color: #fff;
-		font-optical-sizing: auto;
-		font-size: 2rem;
-		font-weight: 700;
-		font-style: normal;
-	}
+  main.main {
+    display: grid;
+    width: 100vw;
+    height: 100vh;
+    align-content: center;
+    place-items: center;
+    font-family: "Pixelify Sans", serif;
+    background-color: #111;
+    color: #fff;
+    font-optical-sizing: auto;
+    font-size: 2rem;
+    font-weight: 700;
+    font-style: normal;
+  }
 
-	h1 {
-		font-weight: 700;
-	}
+  h1 {
+    font-weight: 700;
+  }
 
-	p {
-		font-weight: 400;
-	}
+  p {
+    font-weight: 400;
+  }
 
-	.card {
-		display: grid;
-		align-content: center;
-		place-items: center;
-		background-color: #333;
-		margin: 1rem;
-		padding: 1rem;
-		border-radius: 1rem;
-	}
+  .card {
+    display: grid;
+    align-content: center;
+    place-items: center;
+    background-color: #333;
+    margin: 1rem;
+    padding: 1rem;
+    border-radius: 1rem;
+  }
 </style>
